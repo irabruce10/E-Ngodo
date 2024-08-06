c825333a85997bde4e2235fd1abc5e8f10cbca99
Bruce <irabruce10@gmail.com>
2024-08-06 12:50:24 +0200

    building auth

--- client/src/Pages/Auth/index.tsx
+++ client/src/Pages/Auth/index.tsx
@@ -1,4 +1,21 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/clerk-react";
 export default function Auth() {
-  return <>Auth</>;
+  return (
+    <>
+      <SignedOut>
+        <SignUpButton mode="modal" />
+        <SignInButton mode="modal" />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </>
+  );
 }
-=
