3f0b66f10f809d701c7102841f3d3bfbb417be46
Bruce <irabruce10@gmail.com>
2024-08-06 13:31:06 +0200

    building form

--- client/src/Pages/Dashboard/index.tsx
+++ client/src/Pages/Dashboard/index.tsx
@@ -1,3 +1,16 @@
+import { useUser } from "@clerk/clerk-react";
+import { AccountForm } from "./accountForm";
+import { AccountList } from "./accountList";
+
 export default function Dashboard() {
-  return <>Dashboard</>;
+  const { user } = useUser();
+
+  return (
+    <div className="dashboard-container">
+      <h1>Welcome {user?.firstName}!Here Are your Finances</h1>
+
+      <AccountForm />
+      <AccountList />
+    </div>
+  );
 }
