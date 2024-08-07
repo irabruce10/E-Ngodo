1378de1df46eb9e76c297baddd2f65edd1838f95
Bruce <irabruce10@gmail.com>
2024-08-07 14:12:28 +0200

    building backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@
 
 import express, { Express } from "express";
 import mongoose from "mongoose";
+import ingodoRoutes from './routes/ingodoRoutes'
 
 const app: Express = express();
 
@@ -16,5 +17,8 @@ mongoose
   .connect(mongoURL)
   .then(() => console.log("Connected"))
   .catch((err) => console.log("Failed to connect to MGD", err));
+
+
+  app.use('/finacial',ingodoRoutes)
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
--- server/src/routes/ingodoRoutes.ts
+++ server/src/routes/ingodoRoutes.ts
@@ -1,1 +1,20 @@
- 
+import express, { request, response } from "express";
+
+import ingodoModal from "../schema/ingodoSchema";
+const router = express.Router();
+
+router.get("getAllByUserID:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const record = await ingodoModal.find({ userId: userId });
+
+    if (record.length === 0) {
+      return res.status(404).send("No transactions found for the user");
+    }
+    res.status(200).send(record);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+export default router;
