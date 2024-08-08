d26b086534538af3d75f59b4a3561250660b77a2
Bruce <irabruce10@gmail.com>
2024-08-08 16:15:49 +0200

    building backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,16 @@
 
 import express, { Express } from "express";
 import mongoose from "mongoose";
-import ingodoRoutes from './routes/ingodoRoutes'
+import ingodoRoutes from "./routes/ingodoRoutes";
+
+import cors from "cors";
 
 const app: Express = express();
 
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors);
 
 const mongoURL: string =
   "mongodb+srv://irabruce20:YBpYjjZZQlOXNKvm@e-ngodo.vkm3zlo.mongodb.net/";
@@ -18,7 +21,6 @@ mongoose
   .then(() => console.log("Connected"))
   .catch((err) => console.log("Failed to connect to MGD", err));
 
-
-  app.use('/finacial',ingodoRoutes)
+app.use("/finacial", ingodoRoutes);
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
